9223275e1ecae861c5f6894ae574da4431e139ea
khushiaj <itskhushi.raj@gmail.com>
2021-02-06 03:44:36 +0530

    1.getting optimal path now (but only from level 1 child 2.complete coverage still not covered 3. biderctional_new can be removed later-- its just for testing the code for complete coverage(all nodes)

--- deliverables/bidirectional_search_agent.py
+++ deliverables/bidirectional_search_agent.py
@@ -44,14 +44,14 @@ class BFS:
                 level[node] = level[elem] + 1
 
     def check_intersecting_node(self, visited_src, visited_dest):
-        print(f"Visited vertices from source dictionary - {visited_src}")
-        print(f"Visited vertices from dest dictionary - {visited_dest}")
+        # print(f"Visited vertices from source dictionary - {visited_src}")
+        # print(f"Visited vertices from dest dictionary - {visited_dest}")
+        self.bfs_traversal_intersection =[]
         for key, value in self.adjacency_list.items():
-            if key == "B":
-                pass
             if visited_src[key] and visited_dest[key]:
-                return key
-        return -1
+                self.bfs_traversal_intersection.append(key)
+                #return key
+        return self.bfs_traversal_intersection
 
 
 # Bidirectional Search
@@ -69,6 +69,9 @@ class BidirectionalSearch:
 
         self.bfs_traversal_output_forward = []
         self.bfs_traversal_output_backward = []
+
+        self.bfs_traversal_intersection_path_cost = []
+        self.path_list = {}
 
         self.forward_queue = Queue()
         self.backward_queue = Queue()
@@ -109,28 +112,109 @@ class BidirectionalSearch:
             self.bfs.search("backward", last_elem, self.visited_dest, self.backward_queue, self.parent_dest,
                             self.level_dest)
 
-            self.intersecting = self.bfs.check_intersecting_node(self.visited_src, self.visited_dest)
-            if self.intersecting != -1:
+            print(f"Current level - {self.level_src}")
+            print(f"Visited vertices from source dictionary - {self.visited_src}")
+            print(f"Visited vertices from dest dictionary - {self.visited_dest}")
+
+            self.bfs_traversal_intersection_path_cost = self.bfs.check_intersecting_node(self.visited_src, self.visited_dest)
+            #self.intersecting = self.bfs.check_intersecting_node(self.visited_src, self.visited_dest)
+            if self.bfs_traversal_intersection_path_cost != []:
                 self.bfs_traversal_output_backward.reverse()
                 break
 
-        print(f"BFS forward output is {self.bfs_traversal_output_forward}")
-        print(f"BFS backward output is {self.bfs_traversal_output_backward}")
-        print(f"Intersecting node is {self.intersecting}")
+        #print(f"BFS forward output is {self.bfs_traversal_output_forward}")
+        #print(f"BFS backward output is {self.bfs_traversal_output_backward}")
+        #print(f"Intersecting node is {self.intersecting}")
+        print(f"Intersecting nodelist is {self.bfs_traversal_intersection_path_cost}")
 
     def get_path_and_cost(self, edge_weights, start, dest):
-        if self.intersecting in self.bfs_traversal_output_forward or \
-                self.intersecting in self.bfs_traversal_output_backward:
-            path = self.bfs_traversal_output_forward + self.bfs_traversal_output_backward
-        else:
-            path = self.bfs_traversal_output_forward + [self.intersecting] + self.bfs_traversal_output_backward
-        print(f"Path from {start} to {dest} is {path}")
-        cost = 0
-        for i in range(len(path) - 1):
-            for key, value in edge_weights.items():
-                if (key == path[i] + "" + path[i + 1]) or (key == path[i + 1] + "" + path[i]):
-                    cost = cost + value
-        print(f"Cost to go from {start} to {dest} is {cost}")
+        ctr=-1
+        min_cost=0
+        for i in self.bfs_traversal_intersection_path_cost:
+            self.intersecting=i
+            if self.intersecting in self.bfs_traversal_output_forward or self.intersecting in self.bfs_traversal_output_backward:
+                path = self.bfs_traversal_output_forward + self.bfs_traversal_output_backward
+            else:
+                path = self.bfs_traversal_output_forward + [self.intersecting] + self.bfs_traversal_output_backward
+            self.path_list[++ctr]=path
+            #print(f"Path from {start} to {dest} is {path}")
+            cost = 0
+            for i in range(len(path) - 1):
+                for key, value in edge_weights.items():
+                    if (key == path[i] + "" + path[i + 1]) or (key == path[i + 1] + "" + path[i]):
+                        cost = cost + value
+            if (min_cost==0):
+                min_cost=cost
+                min_path=self.path_list[ctr]
+            if (min_cost!=0 and min_cost>cost):
+                min_cost=cost
+                min_path = self.path_list[ctr]
+        print(f"Optimal Path from {start} to {dest} is {min_path}")
+        print(f"Cost to go from {start} to {dest} is {min_cost}")
+
+class BidirectionalSearch_new:
+
+    def __init__(self, adjacency_list):
+        self.visited_src = {}
+        self.visited_dest = {}
+
+        self.level_src = {}
+        self.level_dest = {}
+
+        self.parent_src = {}
+        self.parent_dest = {}
+
+        self.bfs_traversal_output_forward = []
+        self.bfs_traversal_output_backward = []
+
+        self.bfs_traversal_intersection_path_cost = []
+        self.path_list = {}
+
+        self.forward_queue = Queue()
+        self.backward_queue = Queue()
+
+        self.bfs = BFS(adjacency_list)
+
+        self.intersecting = None
+
+        # initializing dictionaries
+        for node in adjacency_list.keys():
+            self.visited_src[node] = False
+            self.visited_dest[node] = False
+            self.parent_src[node] = None
+            self.parent_dest[node] = None
+            self.level_src[node] = -1  # inf
+            self.level_dest[node] = -1  # inf
+
+        # print(visited),print(parent),print(level)
+
+    def cover_all_nodes(self,edge_weights, start, dest):
+        start_vertex = start
+        self.visited_src[start_vertex] = True
+        self.level_src[start_vertex] = 0
+        self.forward_queue.put(start_vertex)
+
+        dest_vertex = dest
+        self.visited_dest[dest_vertex] = True
+        self.level_dest[dest_vertex] = 0
+        self.backward_queue.put(dest_vertex)
+
+        while not self.forward_queue.empty() and not self.backward_queue.empty():
+            elem = self.forward_queue.get()
+            self.bfs_traversal_output_forward.append(elem)
+            self.bfs.search("forward", elem, self.visited_src, self.forward_queue, self.parent_src, self.level_src)
+
+            last_elem = self.backward_queue.get()
+            self.bfs_traversal_output_backward.append(last_elem)
+            self.bfs.search("backward", last_elem, self.visited_dest, self.backward_queue, self.parent_dest,
+                            self.level_dest)
+
+        print(f"Current levels for source - {self.level_src}")
+        print(f"Current levels for destination - {self.level_dest}")
+
+#insert code to check if level_src and level_dest have have keys/edges at lvel 2--this is itersection point
+#if no--> return no path covering all points exists
+#if yes then find path, using the parent with the intersection point in centre (source to int point+intpont to dest)
 
 
 def main():
@@ -173,7 +257,8 @@ def main():
     bidirectional_search = BidirectionalSearch(adjacency_list.adjacency_list)
     bidirectional_search.search_strategy(source_node, destination_node)
     bidirectional_search.get_path_and_cost(adjacency_list.edge_weights, source_node, destination_node)
-
+    bidirectional_search_new = BidirectionalSearch_new(adjacency_list.adjacency_list)
+    bidirectional_search_new.cover_all_nodes(adjacency_list.edge_weights,source_node, destination_node)
 
 if __name__ == "__main__":
     main()
